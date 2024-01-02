48007cdc3c714f032bee06d868b040e855f94e1b
John <karumanx003@gmail.com>
2024-01-02 16:13:12 +0700

    finish

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -17,3 +17,34 @@ int main(){
     cout << "\nMin = " << B[5];
     return 0;
 }
+
+void stat(const double A[],int N,double B[]){
+    double sum = 0,SD = 0,GM = 1, HM=0;
+    int i = 0;
+    B[5] = A[0];
+    B[6] = A[0];
+    while(i < N){
+        if(B[5] >= A[i]){
+            B[5] = A[i];
+        }
+        if(B[4] <= A[i]){
+            B[4] = A[i];
+        }
+    i++;
+    }
+
+    for(int i = 0;i < N;i++){
+        sum = A[i] + sum;
+        HM =  1/A[i]+HM;
+        GM = GM * A[i];
+    }
+    B[0] = sum / N;
+    for(int i = 0; i < N; ++i) {
+     SD += pow(A[i] - B[0], 2);
+  }
+    B[1] = sqrt(SD/N);
+    B[2] = pow(GM, (float) 1 / N);
+    B[3] = N/HM;
+
+    
+}
